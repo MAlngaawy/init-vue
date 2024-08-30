4fd6165857314d30984d89515b69b0358cc58a5c
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-08-31 01:27:53 +0300

    explain some pinia code

--- src/components/PrimeVue/Components.vue
+++ src/components/PrimeVue/Components.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="mt-10">
-    <h1 class="text-2xl text-white font-bold">Test Primevue Components</h1>
+    <h1 class="text-2xl text-white font-bold">Playground</h1>
+
+    <h1>UI Library</h1>
     <!-- Slider -->
     <ComponentBox title="Slide">
       <Slider v-model="value" class="w-56" />
@@ -11,7 +13,7 @@
       <Checkbox v-model="checked" :binary="true" />
     </ComponentBox>
 
-    <ComponentBox title="Vuex">
+    <ComponentBox title="Pinia State Management">
       <div>
         <p>Count: {{ count }}</p>
         <p>Double Count: {{ doubleCount }}</p>
@@ -25,7 +27,7 @@
   import Slider from 'primevue/slider';
   import Checkbox from 'primevue/Checkbox';
   import Button from 'primevue/button';
-  import { computed, ref } from 'vue';
+  import { ref } from 'vue';
   import ComponentBox from './ComponentBox.vue';
   import { useCounterStore } from '@/stores/counter';
   import { storeToRefs } from 'pinia';
@@ -35,8 +37,6 @@
   const { count, doubleCount } = storeToRefs(store);
 
   const { increment } = store;
-
-  // store.count++
 
   const value = ref(10);
   const checked = ref(false);
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,13 +2,42 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
+  // state (data)
   const count = ref(0)
   
+  // getters (computed properties)
   const doubleCount = computed(() => count.value * 2)
 
+  // actions (methods)
   function increment() {
     count.value++
   }
 
   return { count, doubleCount, increment }
 })
+
+
+
+/**
+ 
+it also can be like that 
+
+
+export const useCounterStore = defineStore('counter', {
+
+  state: () => ({ count: 0, name: 'Eduardo' }),
+
+  getters: {
+    doubleCount: (state) => state.count * 2,
+  },
+  
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+
+})
+
+
+*/
